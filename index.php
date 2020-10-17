8d7639472b77a5ed70bc862d97fce96394aca798
Blances Sanchez <blancessanchez30@gmail.com>
2020-10-17 15:09:50 +0800

    Initialization of files

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+    // example static variable
+    // function myTest() {
+    //     $x = 0;
+    //     echo $x . '<br>';
+    //     $x++;
+    // }
+
+    // myTest();
+    // myTest();
+    // myTest();
+
+    // example operator
+    // echo 1 == 1;
+    // echo '<br>';
+    // echo 1 == '1';
+    // echo '<br>';
+    // echo 1 === 1;
+    // echo '<br>';
+    // echo 1 === '1';
+    // echo '<br>';
+
+    // example arithmetic
+    // $x = 0;
+    // $y = 1;
+    // echo '$x' + '$y';
+    // $xy$y
+    // echo '$x' . '$y';
+
+    // if condition sample
+    $age = 20;
+    if ($age == 21) {
+        echo 'True';
+    }
+
+
